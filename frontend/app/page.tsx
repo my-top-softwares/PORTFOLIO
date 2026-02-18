254b0a96b91900ec8a8146647152c9916cd20359
Faysal-Maxamed <pheymoha17@gmail.com>
2026-02-18 12:16:06 +0300

    ....

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
                         <div className="relative w-full aspect-[4/5] rounded-[48px] overflow-hidden glass p-4 animate-float">
                             <div className="relative w-full h-full rounded-[36px] overflow-hidden  transition-all duration-1000 group">
                                 <Image
-                                    src="/myimage.jpeg"
+                                    src="/myimage.jpe"
                                     alt="Amina - Multimedia Specialist"
                                     fill
                                     className="object-cover  transition-transform duration-1000"
